60bd1b62b0f854da27f1b5d7d2393c69ef2f4818
lBadderl <143137793+lBadderl@users.noreply.github.com>
2024-08-19 18:02:42 -0600

    Update script.js
    Se agregaron las funcionalidades de la cuenta regresiva

--- script.js
+++ script.js
@@ -52,6 +52,44 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+//cuenta regresiva
+//variables
+const $days = document.getElementById('days'),
+$hours = document.getElementById('hours'),
+$minutes = document.getElementById('minutes'),
+$seconds = document.getElementById('seconds'),
+$finalMessage = document.querySelector('.final-sms');
+
+//fecha de interes a futuro (9 21, 2024 16:45:00) (8 19, 2024 17:58:00)
+const countdownDate = new Date('9 21, 2024 16:45:00').getTime();
+
+let interval = setInterval(function() {
+    //fecha actual en milisegundos
+    const now = new Date().getTime();
+
+    let distance = countdownDate - now;
+
+    //Obtencion de los dias
+    let days = Math.floor(distance / (1000 * 60 * 60 *24));
+    //Obtencion de las horas
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    //Obtencion de los minutos
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    //Obtencion de los segundos
+    let seconds = Math.floor((distance % (1000 * 60)) / (1000));
+    
+    //Mostrar los resultados
+    $days.innerHTML = days;
+    $hours.innerHTML = hours;
+    $minutes.innerHTML = minutes;
+    $seconds.innerHTML = ('0' + seconds).slice(-2);
+
+    if(distance < 0){
+        clearInterval(interval);
+        $finalMessage.style.transform = 'translateY(0)';
+    }
+}, 1000);
+
 //funcion para abrir en una nueva pagina
 function newTab(url) {
     window.open(url, '_blank');
